c764525e47419b18b51e02a83580e53bd24b1908
Thierry Thuon <lepiaf@users.noreply.github.com>
2014-09-29 23:37:31 +0200

    specific vhost for symfony

--- start.sh
+++ start.sh
@@ -3,9 +3,10 @@ cd /var/www
 
 composer install
 rm -rf app/cache/*
+php app/console assets:install --symlink web/
 php app/console c:c
 php app/console c:w
 
 service php5-fpm start
 service mongodb start
-service nginx start
+nginx
