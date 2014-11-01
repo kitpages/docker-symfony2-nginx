fcda08378f7d68b5d1beb3380c91c25da4e77537
Manuel Goerlich <m.goerlich1990@gmail.com>
2014-11-01 20:53:39 +0100

    Fixed asset installing, and a permission issue

--- start.sh
+++ start.sh
@@ -3,9 +3,14 @@ cd /var/www
 
 composer install
 rm -rf app/cache/*
-php app/console assets:install --symlink web/
-php app/console c:c
+php app/console c:c --env=prod --no-debug
 php app/console c:w
+php app/console assets:install --symlink web/
+php app/console assetic:dump web/
+chown -R www-data:www-data app/cache
+chown -R www-data:www-data app/logs
+chmod 777 app/cache
+chmod 777 app/logs
 
 service php5-fpm start
 nginx
